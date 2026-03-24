c32bfbf09c6ae18bde1aab4bc0f7be20ee8a0abc
Khrystyna <tkachristina079@gmail.com>
2026-03-24 15:47:27 +0200

    Update workflow

--- playwright.config.js
+++ playwright.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
   testDir: "./e2e",
   use: {
-    baseURL: "http://localhost:5500",
+    baseURL: "http://localhost:5173",
     headless: true,
   },
 });
