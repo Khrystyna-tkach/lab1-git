5bc72078b601965859d544798999aa08a9eacfac
Khrystyna <tkachristina079@gmail.com>
2026-03-24 13:51:24 +0200

    Changed workflow

--- e2e/calculator.spec.js
+++ e2e/calculator.spec.js
@@ -1,25 +1,21 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test';
 
-test("додавання 8 + 2 = 10", async ({ page }) => {
-  await page.goto("http://localhost:5500");
-
-  await expect(page.locator("#display")).toHaveValue("0");
+test('додавання 8 + 2 = 10', async ({ page }) => {
+  await page.goto('http://localhost:5173'); // Змінили порт на 5173
 
   await page.locator('[data-digit="8"]').click();
   await page.locator('[data-op="+"]').click();
   await page.locator('[data-digit="2"]').click();
   await page.locator('[data-action="equals"]').click();
 
-  await expect(page.locator("#display")).toHaveValue("10");
+  await expect(page.locator('#display')).toHaveValue('10');
 });
 
-test("очищення калькулятора повертає 0", async ({ page }) => {
-  await page.goto("http://localhost:5500");
-
-  await expect(page.locator("#display")).toHaveValue("0");
+test('очищення калькулятора повертає 0', async ({ page }) => {
+  await page.goto('http://localhost:5173'); // Змінили порт на 5173
 
   await page.locator('[data-digit="9"]').click();
   await page.locator('[data-action="clear"]').click();
 
-  await expect(page.locator("#display")).toHaveValue("0");
+  await expect(page.locator('#display')).toHaveValue('0');
 });
